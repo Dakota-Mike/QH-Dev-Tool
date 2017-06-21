d5ab90fc849008e53d6ab7f24ddd44d12941f739
Michael <michael@runninggun.com>
2017-06-21 18:36:57 -0400

    Starting in on Write functionality & cleanup

--- TESTSPACE/Program.cs
+++ TESTSPACE/Program.cs
@@ -7,7 +7,7 @@ namespace TESTSPACE
 	{
 		public static void Main (string[] args)
 		{
-			QHSchool p1 = new QHSchool('#', "bicep", "working", "19XX!?" , "School");
+			QHSchool p1 = new QHSchool("#", "bicep", "working", "19XX!?" , "School");
 			Console.WriteLine (p1.save());
 		}
 	}
