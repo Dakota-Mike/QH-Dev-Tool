6f5cfae864e8c4c8c92ac297e2bfa67d25d5bbc0
Michael <michael@runninggun.com>
2017-06-21 16:46:21 -0400

    Baseline
    Initial Commit of Datastructure and command line test file

---
+++ TESTSPACE/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using QH;
+
+namespace TESTSPACE
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			QHSchool p1 = new QHSchool('#', "bicep", "working", "19XX!?" , "School");
+			Console.WriteLine (p1.save());
+			QHInput.Input();
+			QHOutput.Input();
+		}
+	}
+}
